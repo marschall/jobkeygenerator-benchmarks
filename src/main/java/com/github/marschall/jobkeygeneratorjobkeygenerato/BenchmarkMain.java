13e79b67306a2b21b0420e18ab2b9d80ff9d6a53
Philippe Marschall <philippe.marschall@gmail.com>
2021-09-03 10:26:56 +0200

    bump

---
+++ src/main/java/com/github/marschall/jobkeygeneratorjobkeygenerato/BenchmarkMain.java
@@ -0,0 +1,28 @@
+package com.github.marschall.jobkeygeneratorjobkeygenerato;
+
+import static org.openjdk.jmh.results.format.ResultFormatType.TEXT;
+
+import java.io.IOException;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class BenchmarkMain {
+
+  public static void main(String[] args) throws RunnerException, IOException, Exception {
+    Options options = new OptionsBuilder()
+        .include("com\\.github\\.marschall\\.jobkeygeneratorjobkeygenerator\\..*Benchmarks")
+        .forks(1)
+        .warmupIterations(3)
+        .measurementIterations(5)
+        .resultFormat(TEXT)
+        .addProfiler("gc")
+        .addProfiler("jfr", "debugNonSafePoints=true")
+        .build();
+    new Runner(options).run();
+
+  }
+
+}
